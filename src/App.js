f6d3919433216c7e36df93f7c7eeb1d2371554bd
andresaugusto <andres.augusto@icloud.com>
2021-02-12 18:33:52 -0500

    created modal toggle for PieceModal and MenuModal + began implementing styled-components construction for application

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@
 import { useState, useEffect, useMemo } from 'react'
 import { Route, Switch } from 'react-router-dom'
 // import { Frame, Scroll, useCycle } from 'framer'
+import styled from 'styled-components'
 
 
 //  //  //  COMPONENTS    //  //  //
@@ -10,7 +11,6 @@ import { Route, Switch } from 'react-router-dom'
 import ArtistProfile from './components/ArtistProfile'
 import Gallery from './components/Gallery'
 import Home from './components/Home'
-import MenuModal from './components/MenuModal'
 import Nav from './components/Nav'
 import PieceModal from './components/PieceModal'
 
@@ -26,8 +26,21 @@ import { DBURL } from './helperFunctions/config'
 import './App.css'
 
 
+//  //  //  STYLED-COMPONENTS   //  //  //
 
-
+const Body = styled.div`margin-block-start: 0;
+  margin-block-end: 0;
+  margin-inline-start: 0px;
+  margin-inline-end: 0px;
+  background-color: #000000;
+  /* min-height: 100vh; */
+  /* display: flex; */
+  /* flex-direction: column; */
+  /* align-items: center; */
+  /* justify-content: center; */
+  color: rgb(255, 255, 255);
+  font-family: 'New Rocker';
+`
 
 
 //  //  //  FUNCTIONS    //  //  //
@@ -35,8 +48,6 @@ import './App.css'
 export default function App() {
 
 
-
-  
   //  //  //  DATA FETCHING FROM DB   //  //  //
 
   const [artists, setArtists] = useState([]);
@@ -63,47 +74,34 @@ export default function App() {
   }
 
   
-  
-  
   //  //  //  DATA SORTING FOR APP MEMORY   //  //  //
 
   // const providerArtists = useMemo(() => ({ artists, setArtists }), [artists, setArtists])
   // const providerGallery = useMemo(() => ({ gallery, setGallery }), [gallery, setGallery])
 
 
+  //  //  //  PIECEMODAL TOGGLING    //  //  //
 
+  const [showPiece, setShowPiece] = useState(false)
 
-  //  //  //  MODAL TOGGLING    //  //  //
-
-
-  const [showPiece, toggleShowPiece] = useState()
-
-  // const useMenu = () => {
-  //   const [isShowing, setIsShowing] = useCycle(false, true)
-
-  //   funstion
-  // }
-
-  // const usePiece = () => {
-  // }
-
-
+  const openPiece = () => {
+    setShowPiece(prev => !prev)
+  }
 
 
   //  //  //  RENDER    //  //  //
 
   return (
     <div className="App">
-        <Nav />
-        <MenuModal />
-        {/* <PieceModal showPiece={showPiece} hide={toggleShowPiece} /> */}
-        <Switch>
-            <Route exact path='/' component={Home} />
-            <avtcContext.Provider value={ artists }>
-              <Route exact path='/gallery' component={Gallery} />
-            </avtcContext.Provider>
-            <Route exact path='/:artistId' component={ArtistProfile} />
-        </Switch>
+      <Nav />
+      <PieceModal showPiece={showPiece} setShowPiece={setShowPiece} />
+      <Switch>
+          <Route exact path='/' component={Home} />
+          <avtcContext.Provider value={ artists }>
+            <Route exact path='/gallery' component={Gallery} />
+          </avtcContext.Provider>
+          <Route exact path='/:artistId' component={ArtistProfile} />
+      </Switch>
     </div>
   )
 }
--- src/components/PieceModal.js
+++ src/components/PieceModal.js
@@ -1,5 +1,34 @@
-export default function PieceModal() {
+import styled from 'styled-components'
+
+
+//  //  //  STYLED COMPONENTS   //  //  //
+
+const Container = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    width: 100vw;
+
+    z-index: 10;
+    top: 0;
+    position: fixed;
+    background: rgba(0, 0, 0, 0.5);
+    backdrop-filter: blur(15px);
+`
+
+
+
+//  //  //  COMPONENT   //  //  //
+
+export default function PieceModal({ showPiece, setShowPiece}) {
     return (
-        <h1>PieceModal</h1>
+        <>
+            {showPiece ?
+                <Container>
+                    <h1>PieceModal</h1>
+                </Container>
+            : null}
+        </>
     )
 }
