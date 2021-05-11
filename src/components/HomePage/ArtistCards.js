3b5cdd4790f340e9592ac503940a3ccc99a8b68a
Eric Atwell <atwelleric@gmail.com>
2021-05-11 16:55:31 -0500

    artist cards and contact completed

--- src/components/HomePage/ArtistCards.js
+++ src/components/HomePage/ArtistCards.js
@@ -1,213 +1,203 @@
-import { useState, useEffect, useContext } from 'react'
-import { Link } from 'react-router-dom'
-import { useCycle } from 'framer'
-import { motion } from 'framer-motion'
-import styled from 'styled-components'
+import { useState, useEffect, useContext } from "react";
+import { Link } from "react-router-dom";
+import { useCycle } from "framer";
+import { motion } from "framer-motion";
+import styled from "styled-components";
 
 // import { PieceContext, PieceModalToggleContext } from '../../helperFunctions/avtcContext'
-import { DBURL } from '../../helperFunctions/config'
-
+import { DBURL } from "../../helperFunctions/config";
 
 //  //  //  STYLED-COMPONENTS   //  //  //
 
 const Section = styled.section`
-    overflow: hidden;
-    padding: 0;
-    margin: 3vmin;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-`
+  overflow: hidden;
+  padding: 0;
+  margin: 3vmin;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 
 const ItemContainer = styled(motion.ul)`
-    
-    
-    padding: 0;
-    list-style-type: none;
-    margin-block-start: 0;
-    margin-block-end: 0;
-    margin-inline-start: 0;
-    margin-inline-end: 0;
-    padding-inline-start: 0;
+  padding: 0;
+  list-style-type: none;
+  margin-block-start: 0;
+  margin-block-end: 0;
+  margin-inline-start: 0;
+  margin-inline-end: 0;
+  padding-inline-start: 0;
 
-    width: 100vmin;
-    /* height: 90.009vmin; */
-    max-width: 1000px;
-    /* max-height: 1000px; */
-    display: grid;
-    grid-template-columns: repeat(3, 30%);
-    /* grid-template-rows: repeat(2, 1fr); */
-	/* grid-template-columns: repeat(auto-fill, minmax(26vmin, 1fr)); */
-    justify-content: center;
-    align-content: center;
-    grid-gap: 1vmin;
-    /* padding: 2vmin 0 2vmin 0; */
-    /* background: rgba(255, 255, 255, 0.07); */
-    /* background: pink; */
-    /* background: linear-gradient(90deg, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.421), rgba(50, 50, 50, 0.421), rgba(255, 255, 255, 0.171), rgba(255, 255, 255, 0.22), rgba(255, 255, 255, 0.171), rgba(50, 50, 50, 0.421), rgba(0, 0, 0, 0.421), rgba(0, 0, 0, 0)); */
-    /* border-radius: 30px; */
-    -webkit-filter: drop-shadow(0px 2px 2px #000000);
-    filter: drop-shadow(0px 2px 2px #000000);
+  width: 300vmin;
+  /* height: 90.009vmin; */
 
+  /* max width 1000px will keep it same size as the gallery on full screen */
 
-	border-top: 1px solid rgba(255, 255, 255, 0.7);
-`
+  max-width: 1000px;
+  /* max-height: 1000px; */
+  display: grid;
+  grid-template-columns: repeat(3, 30%);
+  /* grid-template-rows: repeat(2, 1fr); */
+  /* grid-template-columns: repeat(auto-fill, minmax(26vmin, 1fr)); */
+  justify-content: center;
+  align-content: center;
+  grid-gap: 1vmin;
+  /* padding: 2vmin 0 2vmin 0; */
+  /* background: rgba(255, 255, 255, 0.07); */
+  /* background: pink; */
+  /* background: linear-gradient(90deg, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.421), rgba(50, 50, 50, 0.421), rgba(255, 255, 255, 0.171), rgba(255, 255, 255, 0.22), rgba(255, 255, 255, 0.171), rgba(50, 50, 50, 0.421), rgba(0, 0, 0, 0.421), rgba(0, 0, 0, 0)); */
+  /* border-radius: 30px; */
+  -webkit-filter: drop-shadow(0px 2px 2px #000000);
+  filter: drop-shadow(0px 2px 2px #000000);
+
+  border-top: 1px solid rgba(255, 255, 255, 0.7);
+  /* background-color: rgba(0, 0, 0, 0.5); */
+`;
 
 const Item = styled(motion.li)`
-    /* width: 25.14433811802233vmin; */
-    /* max-width: 333px; */
-    /* width: 25.14433811802233vmin; */
-    /* max-height: 333px; */
-    /* background-color: rgba(0, 0, 0, 0.5); */
-
-    width: 30vmin;
-    height: 30vmin;
-    max-width: 300px ;
-    max-height: 300px ;
-
-    overflow: hidden;
-`
+  /* width: 25.14433811802233vmin; */
+  /* max-width: 333px; */
+  /* width: 25.14433811802233vmin; */
+  /* max-height: 333px; */
+  /* background-color: rgba(0, 0, 0, 0.5); */
+  width: 30vmin;
+  height: 45vmin;
+  max-width: 300px;
+  max-height: 500px;
+  overflow: hidden;
+`;
 
 const Img = styled(motion.img)`
-    display: block;
+  display: block;
 
-    min-width: inherit;
-    min-height: inherit;
-    max-height: 45vmin;
+  min-width: inherit;
+  min-height: inherit;
+  max-height: 30vmin;
+  margin: 0;
+  padding: 0;
+  /* transform: translate(0%, 0%); */
+  /* position: relative; */
+  left: 50%;
+  top: 0%;
 
-    margin: 0;
-    padding: 0;
-    /* transform: translate(0%, 0%); */
-    /* position: relative; */
-    left: 50%;
-    top: 0%;
-
-    object-fit: cover;
-    -webkit-filter: grayscale(0%);
-    filter: grayscale(0%);
-`
-
+  object-fit: cover;
+  -webkit-filter: grayscale(0%);
+  filter: grayscale(0%);
+`;
 
 const ItemTitle = styled.h1`
-    font-size: 15px;
-    width: 100%;
-`
+  font-size: 3vmin;
+  width: 100%;
+`;
 
 const Li = styled(motion.li)`
-    background-color: rgba(0, 0, 0, 0.3);
-    overflow: hidden;
+  background-color: rgba(0, 0, 0, 0.3);
+  overflow: hidden;
 
-    border: 1px solid rgba(255, 255, 255, 0.3);
-    display: grid;
-    align-items: center;
-    justify-content: center;
-`
+  border: 1px solid rgba(255, 255, 255, 0.3);
+  display: grid;
+  align-items: center;
+  justify-content: center;
+`;
 
 const P = styled.p`
-    color: rgba(255, 255, 255, 0.7);
-`
+  color: rgba(255, 255, 255, 0.7);
+`;
 
+const ArtistTitle = styled.h1`
+  font-size: 5vmin;
+`;
 
 //  //  //  VARIABLES   //  //  //
 
 const ItemContainerAnimation = {
-    hidden: { opacity: 1, scale: 0 },
-    visible: {
-        opacity: 1,
-        scale: 1,
-        transition: {
-        delayChildren: 1.3,
-        staggerChildren: 0.5
-        }
-    }
-};
-  
-const ItemAnimation = {
-    hidden: { y: 20, opacity: 0 },
-    visible: {
-        y: 0,
-        opacity: 1
-    }
+  hidden: { opacity: 1, scale: 0 },
+  visible: {
+    opacity: 1,
+    scale: 1,
+    transition: {
+      delayChildren: 1.3,
+      staggerChildren: 0.5,
+    },
+  },
 };
 
+const ItemAnimation = {
+  hidden: { y: 20, opacity: 0 },
+  visible: {
+    y: 0,
+    opacity: 1,
+  },
+};
 
 //  //  //  FUNCTION    //  //  //
 
 export default function ArtistsPeek() {
+  //  //  //  DATA FETCHING FROM DB   //  //  //
 
+  useEffect(() => {
+    fetchArtists();
+  }, []);
 
-    //  //  //  DATA FETCHING FROM DB   //  //  //
-  
-    useEffect(() => {fetchArtists()}, []);
+  const [artists, setArtists] = useState([]);
 
-    const [ artists, setArtists ] = useState([]);
-  
-    const fetchArtists = async () => {
-      
-      const rtstsData = await fetch(`${DBURL}/artists`)
-      const rtsts = await rtstsData.json();
+  const fetchArtists = async () => {
+    const rtstsData = await fetch(`${DBURL}/artists`);
+    const rtsts = await rtstsData.json();
 
-      setArtists(rtsts)
-      
-    }
+    setArtists(rtsts);
+  };
 
+  // //  //  //  PIECE SETTINGS    //  //  //
 
-    // //  //  //  PIECE SETTINGS    //  //  //
+  // const { setPiece } = useContext(PieceContext)
+  // const { showPiece, setShowPiece } = useContext(PieceModalToggleContext)
 
-    // const { setPiece } = useContext(PieceContext)
-    // const { showPiece, setShowPiece } = useContext(PieceModalToggleContext)
+  // const fetchPiece = async (i) => {
 
-    // const fetchPiece = async (i) => {
-      
-    //     const picData = await fetch(`${DBURL}/gallery/${i}`)
-    //     const pic = await picData.json();
+  //     const picData = await fetch(`${DBURL}/gallery/${i}`)
+  //     const pic = await picData.json();
 
-    //     setPiece(
-    //         {
-    //             title: pic.title,
-    //             slug: pic.slug,
-    //             img: pic.art_image,
-    //             artistSlug: pic.artist_id,
-    //             artistName: pic.artist_name
-    //         }
-    //     )
+  //     setPiece(
+  //         {
+  //             title: pic.title,
+  //             slug: pic.slug,
+  //             img: pic.art_image,
+  //             artistSlug: pic.artist_id,
+  //             artistName: pic.artist_name
+  //         }
+  //     )
 
-    //     setShowPiece(!showPiece)
-    // }
+  //     setShowPiece(!showPiece)
+  // }
 
+  //  //  //  FUNCTIONS    //  //  //
 
-    //  //  //  FUNCTIONS    //  //  //
-
-    return (
-        <>
-            <Section>
-                <ItemContainer
-                    variants={ItemContainerAnimation}
-                    initial="hidden"
-                    animate="visible"
-                    >
-                        {artists.map((i) => (
-                                <Item
-                                    key={i.slug}
-                                    variants={ItemAnimation}
-                                    >
-                                        <Link className='links' to={`/artists/${i.slug}`}>
-                                            <Img 
-                                                src={i.profile_image}
-                                                alt={i.name}
-                                                id={i.slug}
-                                                
-                                                whileHover={{ scale: 1.05 }}
-                                                whileTap={{ scale: 0.9 }}
-                                                transition={{ duration: 0.2 }}
-                                            />
-                                        </Link>
-                                        {/* <h1>{i.name}</h1> */}
-
-                                </Item>
-                        ))}
-                </ItemContainer>
-            </Section>
-        </>
-    )
+  return (
+    <>
+      <ArtistTitle>Resident Artists</ArtistTitle>
+      <Section>
+        <ItemContainer
+          variants={ItemContainerAnimation}
+          initial="hidden"
+          animate="visible"
+        >
+          {artists.map((i) => (
+            <Item key={i.slug} variants={ItemAnimation}>
+              <ItemTitle>{i.name}</ItemTitle>
+              <Link className="links" to={`/artists/${i.slug}`}>
+                <Img
+                  src={i.profile_image}
+                  alt={i.name}
+                  id={i.slug}
+                  whileHover={{ scale: 1.05 }}
+                  whileTap={{ scale: 0.9 }}
+                  transition={{ duration: 0.2 }}
+                />
+              </Link>
+            </Item>
+          ))}
+        </ItemContainer>
+      </Section>
+    </>
+  );
 }
--- src/components/HomePage/ContactUs.js
+++ src/components/HomePage/ContactUs.js
@@ -1,49 +1,139 @@
 //  //  //  FUNCTIONALITY   //  //  //
 
-import styled from 'styled-components'
-import { motion } from 'framer-motion'
-
+import styled from "styled-components";
+import { motion } from "framer-motion";
 
 //  //  //   STYLED-COMPONENTS  //  //  //
 
+const Section = styled.section`
+  overflow: hidden;
+  padding: 0;
+  margin: 3vmin;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
 const A = styled.a`
-color: inherit;
-text-decoration: none;
-`
+  color: inherit;
+  text-decoration: none;
+`;
 
+// initialize Brute Force - Done by Eric Atwell
 
+const ContactContainer = styled.section`
+  padding: 0;
+  list-style-type: none;
+  margin-block-start: 0;
+  margin-block-end: 0;
+  margin-inline-start: 0;
+  margin-inline-end: 0;
+  padding-inline-start: 0;
+
+  width: 150vmin;
+
+  display: grid;
+  grid-template-rows: repeat(2, 40%);
+  grid-template-columns: repeat(3, 33%);
+  grid-gap: 1vmin;
+  -webkit-filter: drop-shadow(0px 2px 2px #000000);
+  filter: drop-shadow(0px 2px 2px #000000);
+
+  border-top: 1px solid rgba(255, 255, 255, 0.7);
+  background-color: rgba(0, 0, 0, 0.5);
+  border-radius: 20px;
+`;
+
+const Social = styled.section`
+  font-size: 2vmin;
+  padding-top: 2vmin;
+`;
+
+const Hours = styled.section`
+  font-size: 2vmin;
+  grid-row: 2;
+`;
+const Phone = styled.section`
+  font-size: 2vmin;
+  grid-row: 2;
+`;
+
+const Location = styled.section`
+  font-size: 2vmin;
+  grid-row: 2;
+  grid-column: 2;
+`;
+
+const Titles = styled.h1`
+  font-size: 3vmin;
+  margin: 0;
+  color: rgba(255, 255, 255, 0.5);
+`;
+
+const SubTitles = styled.p`
+  margin: 0;
+  padding-bottom: 8vmin;
+`;
+
+const ContactTitle = styled.h1`
+  font-size: 3vmin;
+`;
 //  //  //  FUNCTION    //  //  //
 
 export default function ContactUs() {
-    return (
-        <section id='contactUs' className='home-page-sections'>
-                <h1 id='contactUsTitle'>CONTACT US</h1>
-                <div id='contactUsSocialMedia'>
-                    <h5 className='social-media-title'>Social Media</h5>
-                    <div className='social-media-table'>
-                        <h5 className='socialMediaName'>facebook</h5>
-                        <A className='socialMediaHandle' href='https://www.facebook.com/avtatco/'>@avtatco</A>
-                        <h5 className='socialMediaName'>instagram</h5>
-                        <A className='socialMediaHandle' href='https://instagram.com/americanvengeance?igshid=1fzfwzmsrqqkm'>@americanvengeance</A>
-                    </div>
-                </div>
-                <div id='contactUsShopHours'>
-                    <h5 className='shop-hours-title'>Shop Hours</h5>
-                    <h5 className='shop-hours-info'>12:00pm - 7:00pm</h5>
-                </div>
-                <div id='contactUsMainLine'>
-                    <h5 className='main-line-title'>Main Line</h5>
-                    <h5 className='main-line-info'>(262) 619-9520</h5>
-                </div>
-                <div id='contactUsLocation'>
-                    <h5 className='location-title'>Location</h5>
-                    <h5 className='location-info'>
-                        American Vengeance Tattoo Co.<br/>
-                        4615 7th Ave.<br/>
-                        Kenosha, WI<br/>
-                        53140
-                    </h5>
-                </div>
-            </section>
-    )
+  return (
+    <section>
+      <ContactTitle>Contact Us</ContactTitle>
+      <Section>
+        <ContactContainer>
+          <Social>
+            <Titles>facebook</Titles>
+            <A
+              className="socialMediaHandle"
+              href="https://www.facebook.com/avtatco/"
+            >
+              @avtatco
+            </A>
+          </Social>
+          <Social>
+            <Titles>Email</Titles>
+            <A
+              className="socialMediaHandle"
+              href="mailto:atwellcodes@gmail.com?subject=Website%20Contact"
+            >
+              AVTC@gmail.com
+            </A>
+          </Social>
+          <Social>
+            <Titles>instagram</Titles>
+            <A
+              className="socialMediaHandle"
+              href="https://instagram.com/americanvengeance?igshid=1fzfwzmsrqqkm"
+            >
+              @americanvengeance
+            </A>
+          </Social>
+
+          <Social>
+            <Titles>Shop Hours</Titles>
+            <SubTitles className="shop-hours-info">12:00pm - 7:00pm</SubTitles>
+          </Social>
+          <Social>
+            <Titles>Location</Titles>
+            <SubTitles className="location-info">
+              American Vengeance Tattoo Co.
+              <br />
+              4615 7th Ave.
+              <br />
+              Kenosha, WI, 53140
+            </SubTitles>
+          </Social>
+          <Social>
+            <Titles>Main Line</Titles>
+            <SubTitles className="main-line-info">(262) 619-9520</SubTitles>
+          </Social>
+        </ContactContainer>
+      </Section>
+    </section>
+  );
 }
